55d6b7567a4e05e78fb41b5f4cd1a850da48adbc
sdemey00 <DDDemey@outlook.com>
2025-11-01 14:40:39 +0100

    cleaning

--- build/src/parsing/print_scene_infos.d
+++
@@ -1,8 +0,0 @@
-build/src/parsing/print_scene_infos.o: src/parsing/print_scene_infos.c \
-  includes/minirt.h libs/libft/libft.h libs/libft/includes/ft_types.h \
-  libs/libft/includes/ft_char.h libs/libft/includes/ft_cmp.h \
-  libs/libft/includes/ft_convert.h libs/libft/includes/ft_list.h \
-  libs/libft/includes/ft_math.h libs/libft/includes/ft_memory.h \
-  libs/libft/includes/ft_read.h libs/libft/includes/ft_sort.h \
-  libs/libft/includes/ft_string.h libs/libft/includes/ft_write.h \
-  libs/mlx/mlx.h includes/structs.h includes/parsing.h
