4b62076419b7dd8a00d5b7c4b3bed25744e192b5
Sasha Demey <sdemey@student.s19.be>
2025-11-02 23:43:26 +0100

    added format check function (not implemented yet) and nomralize_whitepase

---
+++ src/parsing/isvalid_format.c
@@ -0,0 +1,41 @@
+#include "minirt.h"
+
+t_bool is_valid_int(char *s)
+{
+	if (!s || !*s)
+		return (0);
+	if (*s == '+' || *s == '-')
+		s++;
+	if (!ft_isdigit(*s))
+		return (0);
+	while (*s)
+	{
+		if (!ft_isdigit(*s))
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
+t_bool is_valid_float(char *s)
+{
+	int	dot;
+
+	dot = 0;
+	if (!s || !*s)
+		return (0);
+	if (*s == '+' || *s == '-')
+		s++;
+	while (*s)
+	{
+		if (*s == '.')
+		{
+			if (dot++)
+				return (0);
+		}
+		else if (!ft_isdigit(*s))
+			return (0);
+		s++;
+	}
+	return (1);
+}
