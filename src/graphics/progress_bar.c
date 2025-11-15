0944fef1612d4bf1a26215a938c625ff9ba52c37
Sasha Demey <sdemey@student.s19.be>
2025-11-15 09:09:12 +0100

    added test xpm

---
+++ src/graphics/progress_bar.c
@@ -0,0 +1,17 @@
+#include "minirt.h"
+
+// void	draw_progress_bar(double percent)
+// {
+// 	int	bar_width = WIDTH * percent;
+// 	int	bar_height = 10;
+// 	int	y_start = HEIGHT - bar_height - 5;
+
+// 	for (int y = y_start; y < y_start + bar_height; y++)
+// 	{
+// 		for (int x = 0; x < WIDTH; x++)
+// 		{
+// 			int	color = (x < bar_width) ? 0x00FF00 : 0x202020;
+// 			draw_pixel(d, x, y, color);
+// 		}
+// 	}
+// }
